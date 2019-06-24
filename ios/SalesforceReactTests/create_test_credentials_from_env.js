2180254dc7c500e5304fd6e5866aaf85dbea370b
brandonpage <bpage1@salesforce.com>
2019-06-24 07:45:02 -0700

    Fix test credentials.  Remove "FAKE_FAILURE" tests that RN no longer supports.

--- ios/SalesforceReactTests/create_test_credentials_from_env.js
+++ ios/SalesforceReactTests/create_test_credentials_from_env.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 require("fs").writeFile("test_credentials.json",
-                        `{process.env.TEST_CREDENTIALS}`,
+                        `${process.env.TEST_CREDENTIALS}`,
                         function (err) {
                             if (err) throw err;
                         });
