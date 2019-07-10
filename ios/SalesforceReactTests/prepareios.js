cf41676d73a1ffe255e01c3e8a78c757c75b5195
Wolfgang Mathurin <wmathurin@salesforce.com>
2019-07-09 21:42:30 -0600

    Updating iOS RN tests for 0.60
    Because 0.60 has a lean core (see https://github.com/facebook/react-native/pull/23721), test runner is no longer part of the files downloaded through npm and therefore has to be downloaded by cloning react-native repo.

--- ios/SalesforceReactTests/prepareios.js
+++ ios/SalesforceReactTests/prepareios.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 
+const path = require('path')
 var execSync = require('child_process').execSync;
+var rimraf = require('rimraf');
 
 console.log('=== Installing npm dependencies');
 execSync('npm install', {stdio:[0,1,2]});
+
+console.log('=== Getting react native git repo (for test runner classes)');
+rimraf.sync('react-native');
+const rnVersion = require(path.join(__dirname, '..', '..', 'package.json')).peerDependencies['react-native']
+execSync('git clone --branch v' + rnVersion + ' --single-branch --depth 1 https://github.com/facebook/react-native', {stdio:[0,1,2]});
 
 console.log('=== Installing sdk dependencies');
 execSync('node ./updatesdk.js', {stdio: [0,1,2]});
