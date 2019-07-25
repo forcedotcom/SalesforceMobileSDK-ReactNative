0bf2eb6e93774ee0107317791016c57c3f6f1d48
Wolfgang Mathurin <wmathurin@salesforce.com>
2019-07-25 16:37:56 -0600

    require(rimraf) should follow npm i

--- ios/SalesforceReactTests/prepareios.js
+++ ios/SalesforceReactTests/prepareios.js
@@ -2,10 +2,11 @@
 
 const path = require('path')
 var execSync = require('child_process').execSync;
-var rimraf = require('rimraf');
 
 console.log('=== Installing npm dependencies');
 execSync('npm install', {stdio:[0,1,2]});
+
+var rimraf = require('rimraf');
 
 console.log('=== Getting react native git repo (for test runner classes)');
 const rnVersion = require(path.join(__dirname, '..', '..', 'package.json')).peerDependencies['react-native']
