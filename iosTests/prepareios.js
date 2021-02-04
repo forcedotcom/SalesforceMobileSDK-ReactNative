ffd4cff721dca0fd80a17eb179c74b9f1dccbf27
Wolfgang Mathurin <wmathurin@salesforce.com>
2021-02-04 12:58:55 -0800

    Simplifying podfile for RN tests app
    Similar to https://github.com/forcedotcom/SalesforceMobileSDK-Templates/pull/304
    Had to move stuff around a bit to more closely mirror templates

--- iosTests/prepareios.js
+++ iosTests/prepareios.js
@@ -10,7 +10,7 @@ rimraf.sync('yarn.lock');
 execSync('yarn install', {stdio:[0,1,2]});
 
 console.log('=== Getting react native git repo (for test runner classes)');
-const rnVersion = require(path.join(__dirname, '..', '..', 'package.json')).peerDependencies['react-native']
+const rnVersion = require(path.join(__dirname, '..', 'package.json')).peerDependencies['react-native']
 execSync('git clone --branch v' + rnVersion + ' --single-branch --depth 1 https://github.com/facebook/react-native', {stdio:[0,1,2]});
 rimraf.sync('RCTTest');
 execSync('mv react-native/RNTester/RCTTest .');
@@ -22,7 +22,7 @@ console.log('=== Installing sdk dependencies');
 execSync('node ./updatesdk.js', {stdio: [0,1,2]});
 
 console.log('=== Installing pod dependencies');
-execSync('pod update', {stdio:[0,1,2]});
+execSync('pod update', {stdio:[0,1,2], cwd:'ios'});
 
 console.log('=== Creating test_credentials.json');
 execSync('touch test_credentials.json', {stdio:[0,1,2]});
