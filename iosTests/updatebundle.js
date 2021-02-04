8f9a657ff1593e78db694762e8aee90093c57c4e
Wolfgang Mathurin <wmathurin@salesforce.com>
2021-02-04 14:00:37 -0800

    Fixing bad path in update bundle

--- iosTests/updatebundle.js
+++ iosTests/updatebundle.js
@@ -2,4 +2,4 @@
 
 var execSync = require('child_process').execSync;
 
-execSync('./node_modules/.bin/react-native  bundle --platform ios --dev true --entry-file node_modules/react-native-force/test/alltests.js --bundle-output SalesforceReactTests/index.ios.bundle --assets-dest SalesforceReactTests/', {stdio:[0,1,2]});
+execSync('./node_modules/.bin/react-native  bundle --platform ios --dev true --entry-file node_modules/react-native-force/test/alltests.js --bundle-output ios/index.ios.bundle --assets-dest ios/', {stdio:[0,1,2]});
