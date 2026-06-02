1ce3dca796deaacd6245ddc8560e7c34f0c8c14a
Wolfgang Mathurin <wmathurin@salesforce.com>
2026-06-02 16:06:14 -0700

    Suppress notification popups in test app
    - iOS: removed all push notification code from AppDelegate
    - Android: auto-grant POST_NOTIFICATIONS permission in UI test rule

--- androidTests/android/app/src/androidTest/java/com/salesforce/androidsdk/reactnative/BaseReactNativeTest.kt
+++ androidTests/android/app/src/androidTest/java/com/salesforce/androidsdk/reactnative/BaseReactNativeTest.kt
@@ -10,10 +10,20 @@ import androidx.test.uiautomator.Until
 import org.junit.Assert.assertTrue
 import org.junit.Assert.fail
 import org.junit.Before
+import org.junit.Rule
+import androidx.test.rule.GrantPermissionRule
+import android.os.Build
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 abstract class BaseReactNativeTest {
+
+    @get:Rule
+    val permissionRule: GrantPermissionRule = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        GrantPermissionRule.grant(android.Manifest.permission.POST_NOTIFICATIONS)
+    } else {
+        GrantPermissionRule.grant()
+    }
 
     private val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
--- iosTests/ios/SalesforceReactTestApp/AppDelegate.swift
+++ iosTests/ios/SalesforceReactTestApp/AppDelegate.swift
@@ -9,42 +9,30 @@ import SalesforceSDKCore
 class AppDelegate: UIResponder, UIApplicationDelegate {
   private var launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   var window: UIWindow?
-  
+
   var reactNativeDelegate: ReactNativeDelegate?
   var reactNativeFactory: RCTReactNativeFactory?
-  
+
   override init() {
     super.init()
-    
-    // Need to use SalesforceReactSDKManager in Salesforce Mobile SDK apps using React Native
     SalesforceReactSDKManager.initializeSDK()
   }
-  
+
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
     self.launchOptions = launchOptions
-    
+
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
-    
+
     reactNativeDelegate = delegate
     reactNativeFactory = factory
-    
+
     window = UIWindow(frame: UIScreen.main.bounds)
-    
-    // If you wish to register for push notifications uncomment the line
-    // below.  Note that if you want to receive push notifications from
-    // Salesforce you will also need to implement the
-    // application:didRegisterForRemoteNotificationsWithDeviceToken:
-    // method below.
-    
-    // Uncomment the code below to see how you can customize the color,
-    // text color, font and font size of the navigation bar.
-    customizeLoginView()
-    
+
     AuthHelper.loginIfRequired() {
       factory.startReactNative(
         withModuleName: "SalesforceReactTestApp",
@@ -52,56 +40,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         launchOptions: launchOptions
       )
     }
-    
+
     return true
   }
-  
-    UNUserNotificationCenter.current().requestAuthorization(options: [.sound, .alert, .badge]) { granted, error in
-      if (granted) {
-        DispatchQueue.main.async {
-          PushNotificationManager.shared.registerForRemoteNotifications()
-        }
-      }
-      
-      if (error != nil) {
-        SalesforceLogger.e(AppDelegate.self, message: "Push notification authorization error: \(String(describing: error))")
-      }
-    }
-  }
-  
-  private func customizeLoginView() {
-    let loginViewConfig = SalesforceLoginViewControllerConfig()
-    
-    // Set `showsSettingsIcon` to false if you want to hide the settings icon on the nav bar
-    loginViewConfig.showsSettingsIcon = true
-    
-    // Set `showsNavigationBar` to false if you want to hide the top bar
-    loginViewConfig.showsNavigationBar = true
-    
-    loginViewConfig.navigationBarColor = UIColor.init(red: 0.051, green:0.765, blue:0.733, alpha:1.0)
-    loginViewConfig.navigationTitleColor = UIColor.white
-    loginViewConfig.navigationBarFont = UIFont.init(name: "Helvetica", size:16.0)
-    
-    UserAccountManager.shared.loginViewControllerConfig = loginViewConfig
-  }
-  
-  
-  private func didRegisterForRemoteNotifications(withDeviceToken deviceToken:Data) {
-    
-    PushNotificationManager.shared.didRegisterForRemoteNotifications(withDeviceToken: deviceToken)
-    
-    if UserAccountManager.shared.currentUserAccount?.credentials.accessToken != nil {
-      PushNotificationManager.shared.registerSalesforceNotifications(completionBlock: nil, failBlock: nil)
-    }
-  }
-  
 }
 
 class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
   override func sourceURL(for bridge: RCTBridge) -> URL? {
     self.bundleURL()
   }
-  
+
   override func bundleURL() -> URL? {
     Bundle.main.url(forResource: "index.ios", withExtension: "bundle")
   }
