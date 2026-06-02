df55c4a0103b5f21733f95b8d6c18f06efe66135
Wolfgang Mathurin <wmathurin@salesforce.com>
2026-06-02 10:28:03 -0700

    Remove old test infrastructure, simplify to template pattern
    iOS:
    - Remove RCTTest/ directory (custom test runner pod)
    - Remove React-RCTTest from Podfile
    - Remove old XCTest files (ReactTestCase, React*Tests.m)
    - Podfile: all pods in app target, test target inherits
    
    Android:
    - Remove SalesforceTestBridge, ReactNativeTestHost, ReactTestActivity,
      ReactActivityTestDelegate, TestResult, SalesforceReactTestPackage,
      DebugSalesforceReactPackage
    - Remove old instrumentation test classes
    - Rewrite SalesforceReactTestApp.kt to match template pattern
    - Add MainActivity.kt (standard SalesforceReactActivity)
    - Update AndroidManifest for new MainActivity
    
    JS:
    - Remove src/react.force.test.tsx (replaced by test/testRunner.js)
    - Remove forceTest export from src/index.ts

---
+++ androidTests/android/app/src/main/java/com/salesforce/androidsdk/reactnative/util/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.salesforce.androidsdk.reactnative.util
+
+import com.salesforce.androidsdk.reactnative.ui.SalesforceReactActivity
+
+class MainActivity : SalesforceReactActivity()
--- androidTests/android/app/src/main/java/com/salesforce/androidsdk/reactnative/util/SalesforceReactTestApp.kt
+++ androidTests/android/app/src/main/java/com/salesforce/androidsdk/reactnative/util/SalesforceReactTestApp.kt
@@ -1,46 +1,32 @@
-/*
- * Copyright (c) 2017-present, salesforce.com, inc.
- * All rights reserved.
- * Redistribution and use of this software in source and binary forms, with or
- * without modification, are permitted provided that the following conditions
- * are met:
- * - Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- * - Neither the name of salesforce.com, inc. nor the names of its contributors
- * may be used to endorse or promote products derived from this software without
- * specific prior written permission of salesforce.com, inc.
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
 package com.salesforce.androidsdk.reactnative.util
 
 import android.app.Application
+import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-import com.salesforce.androidsdk.analytics.EventBuilderHelper
 import com.salesforce.androidsdk.reactnative.app.SalesforceReactSDKManager
-import com.salesforce.androidsdk.util.test.TestCredentials
 
 class SalesforceReactTestApp : Application(), ReactApplication {
 
-    override val reactNativeHost: ReactNativeHost = ReactNativeTestHost(this)
+    override val reactNativeHost: ReactNativeHost =
+        object : DefaultReactNativeHost(this) {
+            override fun getPackages(): List<ReactPackage> =
+                PackageList(this).packages
+
+            override fun getJSMainModuleName(): String = "index"
+
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        }
 
     override val reactHost: ReactHost
         get() = getDefaultReactHost(applicationContext, reactNativeHost)
@@ -49,8 +35,6 @@ class SalesforceReactTestApp : Application(), ReactApplication {
         super.onCreate()
         SoLoader.init(this, OpenSourceMergedSoMapping)
         load()
-        EventBuilderHelper.enableDisable(false)
-        SalesforceReactSDKManager.initReactNative(applicationContext, ReactTestActivity::class.java)
-        TestCredentials.init(this)
+        SalesforceReactSDKManager.initReactNative(applicationContext, MainActivity::class.java)
     }
 }
