079f307657bff1b7e152a5517467c1980192c7e8
Wolfgang Mathurin <wmathurin@salesforce.com>
2026-06-02 13:12:56 -0700

    Fix Android blank screen: override getMainComponentName and createReactActivityDelegate

--- androidTests/android/app/src/main/java/com/salesforce/androidsdk/reactnative/util/MainActivity.kt
+++ androidTests/android/app/src/main/java/com/salesforce/androidsdk/reactnative/util/MainActivity.kt
@@ -1,5 +1,21 @@
 package com.salesforce.androidsdk.reactnative.util
 
+import android.os.Bundle
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
 import com.salesforce.androidsdk.reactnative.ui.SalesforceReactActivity
 
-class MainActivity : SalesforceReactActivity()
+class MainActivity : SalesforceReactActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null)
+    }
+
+    override fun getMainComponentName() = "SalesforceReactTestApp"
+
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    override fun shouldAuthenticate() = true
+}
