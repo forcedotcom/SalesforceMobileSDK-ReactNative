8af858e96a3169c730516265b07e500e4e5d3e4d
Brandon Page <bpage@salesforce.com>
2026-06-04 16:45:51 -0700

    Bump Android minSdk to 31 (#460)
    Bump Android minSdk to 31

--- androidTests/android/app/build.gradle.kts
+++ androidTests/android/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.salesforce.androidsdk.reactnative.tests"
-        minSdk = 28
+        minSdk = 31
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
